bc1ab8cdb1b92346ddd7f7a59ae38f361508149f
wjurasz <juraszwojtek03@gmail.com>
2025-04-07 18:40:51 +0200

    new model, app working now

--- Data/Migrations/20250407163235_inital.Designer.cs
+++ Data/Migrations/20250407163235_inital.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjektJezykiWebowe.Data;
 
@@ -11,9 +12,11 @@ using ProjektJezykiWebowe.Data;
 namespace ProjektJezykiWebowe.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250407163235_inital")]
+    partial class inital
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
